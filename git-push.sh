86eff5dd813ae42baddfb1c4af7198c64e85dcee
ganesh <you@example.com>
2021-02-08 18:48:16 +0530

    test

--- git-push.sh
+++ git-push.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -e
 commit_message="sample message"
 git checkout master
-echo "ganesh" > ganesh_test.txt
+echo "ganesh $(uuidgen)" > ganesh_test.txt
 git add .
 git commit -m "$commit_message"
 git push origin master
 
 git checkout ganesh-test
-echo "ganesh" > ganesh_test.txt
+echo "ganesh $(uuidgen)" > ganesh_test.txtt
 git add .
 git commit -m "$commit_message"
 git push origin ganesh-test
